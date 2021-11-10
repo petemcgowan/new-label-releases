cf745584540be43a8c1abbeebf8edb121da9cf34
Pete <peteburquette@gmail.com>
2021-11-10 01:08:36 +0000

    adding login, auth, register etc

--- client/src/reducers/authReducer.ts
+++ client/src/reducers/authReducer.ts
@@ -6,24 +6,24 @@ import {
   LOGIN_FAIL,
   LOGOUT_SUCCESS,
   REGISTER_SUCCESS,
-  REGISTER_FAIL
-} from '../actions/types';
+  REGISTER_FAIL,
+} from "../actions/types";
 
 const initialState = {
-  token: localStorage.getItem('NLRtoken'),
-  email: localStorage.getItem('NLRemail'),
+  token: localStorage.getItem("NLRtoken"),
+  email: localStorage.getItem("NLRemail"),
   isAuthenticated: null,
   isLoading: false,
-  user: null
+  user: null,
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action: any) {
   switch (action.type) {
     case USER_LOADING:
       console.log("authReducer, USER_LOADING");
       return {
         ...state,
-        isLoading: true
+        isLoading: true,
       };
     case USER_LOADED:
       console.log("authReducer, USER_LOADED");
@@ -31,32 +31,33 @@ export default function(state = initialState, action) {
         ...state,
         isAuthenticated: true,
         isLoading: false,
-        user: action.payload
+        user: action.payload,
       };
     case LOGIN_SUCCESS:
     case REGISTER_SUCCESS:
       console.log("authReducer, REGISTER_SUCCESS");
-      localStorage.setItem('NLRtoken', action.payload.token);
-      localStorage.setItem('NLRemail', action.payload.email);
+      // localStorage.setItem("NLRemail", data.user.email);
+      localStorage.setItem("NLRtoken", action.payload.token);
+      localStorage.setItem("NLRemail", action.payload.email);
       return {
         ...state,
         ...action.payload,
         isAuthenticated: true,
-        isLoading: false
+        isLoading: false,
       };
     case AUTH_ERROR:
     case LOGIN_FAIL:
     case LOGOUT_SUCCESS:
     case REGISTER_FAIL:
       console.log("authReducer, REGISTER_FAIL");
-      localStorage.removeItem('NLRtoken');
-      localStorage.removeItem('NLRemail');
+      localStorage.removeItem("NLRtoken");
+      localStorage.removeItem("NLRemail");
       return {
         ...state,
         token: null,
         user: null,
         isAuthenticated: false,
-        isLoading: false
+        isLoading: false,
       };
     default:
       return state;
