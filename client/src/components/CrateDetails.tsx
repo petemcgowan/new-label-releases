16b90676ff1627a13936cfd8f2f63b2e13d58518
Pete <peteburquette@gmail.com>
2021-11-16 08:22:23 +0000

    adding track name

---
+++ client/src/components/CrateDetails.tsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { IRecord} from "../cratestate/RCState";
+import { RCContext } from "../cratestate/RCContext";
+import "../styles/uiElements.scss";
+import { deleteAxiosRecord } from "../cratestate/RCActions";
+
+interface CrateRowProps {
+  crateItem: IRecord
+}
+
+const CrateDetails = ({ crateItem}: CrateRowProps ) => {
+  // const { deleteRecordCrate } = useContext(RecordCrateContext);
+  const { dispatchRC } = useContext(RCContext);
+  return (
+    <tr key={crateItem.id.toString()}>
+      <td
+        onClick={() => {
+          // deleteRecordCrate(crateItem.id);
+          deleteAxiosRecord(dispatchRC, crateItem.id);
+        }}
+      >
+        <div className="releaseName">{crateItem.trackName}</div>
+        <div className="artists">{crateItem.releaseName}</div>
+        <div className="artists">{crateItem.artists}</div>
+      </td>
+    </tr>
+  );
+};
+
+export default CrateDetails;
