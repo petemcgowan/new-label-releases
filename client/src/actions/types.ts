6d39e4e9645cbe4e5135d12db7d786d87cd25358
Pete <peteburquette@gmail.com>
2021-11-26 11:07:38 +0000

    type consts

---
+++ client/src/actions/types.ts
@@ -0,0 +1,43 @@
+import App from "../App";
+
+export const GET_ITEMS = "GET_ITEMS";
+export const ADD_ITEM = "ADD_ITEM";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const ITEMS_LOADING = "ITEMS_LOADING";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+// labels
+export const SEARCH_LABELS = "SEARCH_LABELS";
+export const DELETE_LABEL = "DELETE_LABEL";
+export const ADD_LABEL = "ADD_LABEL";
+export const LABELS_ERROR = "LABELS_ERROR";
+
+// //record crate
+// export const SEARCH_RECORD_CRATE = "SEARCH_RECORD_CRATE";
+// export const DELETE_RECORD_CRATE = "DELETE_RECORD_CRATE";
+// export const ADD_RECORD_CRATE = "ADD_RECORD_CRATE";
+// export const RECORD_CRATE_ERROR = "RECORD_CRATE_ERROR";
+
+//global / app
+export const GLOBAL_ERROR = "GLOBAL_ERROR";
+export const GET_LOGGED_IN_EMAIL = "GET_LOGGED_IN_EMAIL";
+export const ADD_LOGGED_IN_EMAIL = "ADD_LOGGED_IN_EMAIL";
+
+// releases
+export const ADD_DUMMY_RELEASE = "ADD_DUMMY_RELEASE";
+export const ADD_RELEASES = "ADD_RELEASES";
+export const REMOVE_RELEASE = "REMOVE_RELEASE";
+export const SET_VOLUME = "SET_VOLUME";
+export const SET_DURATION = "SET_DURATION";
+export const SET_CURRENT_TIME = "SET_CURRENT_TIME";
+export const PLAY = "PLAY";
+export const PAUSE = "PAUSE";
