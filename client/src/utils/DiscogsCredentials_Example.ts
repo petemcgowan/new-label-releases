20e5d06766a2ec8755e2c7af604b81b46078b466
Pete <peteburquette@gmail.com>
2021-12-06 21:55:06 +0000

    discogs key example

---
+++ client/src/utils/DiscogsCredentials_Example.ts
@@ -0,0 +1,11 @@
+// 1.  Rename this file to DiscogsCredentials.ts
+// 2.  Replace the API_KEY and SECRET values with whatever yours are
+
+const DiscogsCredentials = () => {
+  return {
+    API_KEY: "apiKey",
+    SECRET: "apiSecret",
+  };
+};
+
+export { DiscogsCredentials };
